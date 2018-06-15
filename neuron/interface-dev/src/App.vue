6df0615e3957eed59ed528c49786bd84c46b60f1
tomforge <fooguowei3@gmail.com>
2018-06-16 01:23:07 +0800

    Frontend experiments

--- neuron/interface-dev/src/App.vue
+++ neuron/interface-dev/src/App.vue
@@ -1,71 +1,68 @@
 <template>
-  <v-app>
+  <v-app
+    id="inspire"
+    dark
+  >
     <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
-      enable-resize-watcher
       fixed
+      clipped
       app
     >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text" @click="">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
           </v-list-tile-content>
+        </v-list-tile>
+        <v-subheader class="mt-3 grey--text text--darken-1">HELLO WORLD</v-subheader>
+		 <v-list-tile v-for="view in views" :key="view.text" @click="">
+			<v-list-tile-content>
+				<router-link :to="view.link" tag="v-list-tile-title" style="cursor:pointer">
+					{{ view.text }}
+				</router-link>
+			</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
+      color="orange darken-1"
+      dense
+      fixed
+      clipped-left
       app
-      :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Neuron</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+	  
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-layout>
     </v-toolbar>
     <v-content>
-      <router-view/>
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+			<router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -75,17 +72,16 @@ export default {
   name: 'App',
   data () {
     return {
-      clipped: false,
       drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      items: [
+        { icon: 'trending_up', text: 'Adam\'s' },
+        { icon: 'featured_play_list', text: 'Mad' },
+        { icon: 'watch_later', text: 'Aces' }
+      ],
+	  views: [
+		{ link: "/", text: "Home" },
+		{ link: "/about", text: "About" }
+	  ]
     }
   }
 }
