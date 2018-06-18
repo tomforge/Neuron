42d6f172c6fccb63111a096181072beb13eae1cf
Adam <adamwth@hotmail.com>
2018-06-18 19:49:38 +0800

    tried drawing with dagre-d3

--- neuron/interface-dev/src/views/Draw.vue
+++ neuron/interface-dev/src/views/Draw.vue
@@ -4,57 +4,67 @@
   <v-navigation-drawer v-model="drawer" fixed clipped app permanent stateless>
     <v-text-field class="pa-2" :append-icon-cb="() => {}" placeholder="Search"
        single-line append-icon="search" color="white" hide-details
-       v-model="searchStr"></v-text-field>
+       v-model="searchStr" @input="filterSearchResults"></v-text-field>
     <v-list>
-      <template v-for="item in filteredNodeTypes">
-        <v-list-tile :key="item" @click="" color="grey lighten-1">
+      <template v-for="item in searchRes">
+        <v-list-tile :key="item" @click="">
           <v-list-tile-content>
-            <v-list-tile-title v-html="$options.filters.highlight(item, searchStr, 'white')">{{item}}</v-list-tile-title>
+            <v-list-tile-title>{{item}}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-divider></v-divider>
       </template>
     </v-list>
   </v-navigation-drawer>
-  <svg id="board" width="1280px" height="960px"></svg>
+    <svg id="board" width=1280 height=720 fill="white"></svg>
 </div>
 </template>
 <script>
-
 import * as d3 from "d3";
-import { mapState } from "vuex";
-
+import * as dagreD3 from "dagre-d3"
 export default {
   name: 'Draw',
   data() {
     return {
       drawer: true,
+      graph: "",
       searchStr: "",
+      apis: ["adam's", "mad", "aces", "add", "subtract", "mult", "matmul", "dot"],
+      searchRes: ["adam's", "mad", "aces", "add", "subtract", "mult", "matmul", "dot"]
     }
   },
-  computed: {
-    // vuex states
-    ...mapState({
-      nodeTypes: "nodeTypes"
-    }),
-
-    filteredNodeTypes() {
-      if(!this.searchStr) {
-        return this.nodeTypes;
-      } else {
-        return this.nodeTypes.filter(
-          // Return only word-boundary matches (e.g. don't return "add" if
-          // searching for "d")
-          nodeType => (nodeType.search("\\b" + this.searchStr) !== -1)
-        );
-      }
-    }
-  },
-
   mounted() {
-    this.initBoard();
-  },
+    this.initDagreD3Graph();
 
+    let g = this.graph;
+
+    g.setNode("a", {label: "a"});
+    g.setNode("b", {label: "b"});
+    g.setEdge("a", "b", {label: "a to b"});
+
+    // Create the renderer
+    var render = new dagreD3.render();
+
+    // Set up an SVG group so that we can translate the final graph.
+    var svg = d3.select("svg"),
+        svgGroup = svg.append("g");
+
+    // Set up zoom support
+    var zoom = d3.zoom().on("zoom", function() {
+          svgGroup.attr("transform", d3.event.transform);
+        });
+    svg.call(zoom);
+
+    // Run the renderer. This is what draws the final graph.
+    render(svgGroup, g);
+
+    // Center the graph
+    var initialScale = 0.75;
+    svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr("width") - g.graph().width * initialScale) / 2, 20).scale(initialScale));
+
+    svg.attr('height', g.graph().height * initialScale + 40);;
+    
+  },
   methods: {
     initBoard() {
       let svg = d3.select("svg").call(d3.zoom().on("zoom", function () {
@@ -66,14 +76,17 @@ export default {
         .attr("cy", document.body.clientHeight / 2)
         .attr("r", 50)
         .style("fill", "#FFFFFF")
-    }
-  },
-
-  filters: {
-    highlight: function(str, toMatch, color) {
-      var matcher = new RegExp(toMatch, "i");
-      return str.replace(matcher,
-        matched => ("<span style=\"color:" + color + "\">" + matched + "</span>"));
+    },
+    filterSearchResults() {
+      this.searchRes = this.apis.filter(this.searchMatch);
+    },
+    searchMatch(str) {
+      return str.search(this.searchStr) !== -1;
+    },
+    initDagreD3Graph() {
+      this.graph = new dagreD3.graphlib.Graph()
+          .setGraph({})
+          .setDefaultEdgeLabel(function() { return {}; });
     }
   }
 }
